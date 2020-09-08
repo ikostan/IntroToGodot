a0ff80d63853c6e9317267b8a52a9c803a073389
Egor Kostan <igorkostan@gmail.com>
2020-09-07 18:18:36 -0700

    Some comments added

--- Hazards/Lighting.gd
+++ Hazards/Lighting.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const SPEED = 300
+const SPEED = 350
 
 
 func _ready():
@@ -15,15 +15,27 @@ func _process(delta):
 
 
 func _on_VisibilityNotifier2D_screen_exited():
+	"Call destructor in case Lighting exited visible screen"
+	# Debug only
+	var globals = preload("res://Levels/GameState.gd")
+	if globals.get('DEBUG'):
+		print(self.name, ' > ', '_on_VisibilityNotifier2D_screen_exited')
 	queue_free()
 
 
 func _on_LightingArea2D_body_entered(body):
+	"""Lighting listener: 
+		1. hurt player if colided
+		2. call destructor in case of any collision: terrain or player
+	"""
 	# Debug only
 	var globals = preload("res://Levels/GameState.gd")
 	if globals.get('DEBUG'):
 		print(self.name, ' > ', '_on_LightingArea2D_body_entered')
 	
+	# hurt player if colided
 	if body.name == 'Player':
 		get_tree().call_group('GameState', 'applay_damage')
+	
+	# call destructor
 	queue_free()
