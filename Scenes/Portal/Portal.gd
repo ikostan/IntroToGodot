83379ae292a2a37a898e6b8d6810c46187c519cc
Egor Kostan <igorkostan@gmail.com>
2020-09-07 21:11:02 -0700

    Logs added

--- Scenes/Portal/Portal.gd
+++ Scenes/Portal/Portal.gd
@@ -2,5 +2,10 @@ extends Node2D
 
 
 func _on_Area2D_body_entered(body):
+	# DEBUG only
+	# load GLOBALS
+	var globals = preload("res://Levels/GameState.gd")
+	if globals.get('DEBUG'):
+		print(self.name, ' > ', '_on_Area2D_body_entered', ' > player entered Portal')
 	$Area2D/AudioStreamPlayer2D.play()
 	get_tree().call_group('GameState', 'win_game')
